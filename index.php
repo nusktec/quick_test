8ccb4a445c2c287488cf4ef24a792f69f6cccddf
Revelation A.F <nusktecsoft@gmail.com>
2019-07-26 07:00:56 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+TEST READ ME.
+Read the instruction carefully before taken any action.
+
+////////////////////////////////////////////////////////
+This test does not specify any language for you but has a scope limit and language preffered which are
+MYSQL, PHP, JAVASCRIPT, REACT JS, ANDROID, HTML5 AND IOS
+
+Your free to build the test using single language provided all the functions exist, it doesnt matter how many language in combinations.
+
+//////////////////////////////////////////////////////////
+
+//With the task below, you can create any kind of project comfortable for you but not outside the scope
+
+Task 1: Create a github account fork this repository
+Task 2: Create mysql database with the follwing fields (ID, NAME, EMAIL, PASSWORD, PICTURE, DEFAULT) "table name: sqtdb_test"
+Task 3: Create a server side script to handle your request (Any server side language is accepted)
+Task 4: Create UI with any of this (HTML, Andorid, iOS) "You can use as many you want"
+Task 5: Using any javascript framework to communicate between (Server side and front side (UI)) "Communication method: JSON DataType"
+
+//Example of project: Maybe a (Login system, UserDetail viewer etc) "You can build any but not outside the task scope"
+//Project should have a section where you can update your data as needed.
+
+Note: You must not add to the project scope and you'r free to debug as it may contain bugs
+
+Time Frame: 12hrs
+
+Communication channel: Reply via any channel you received this.
